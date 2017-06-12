88205e191c56333e734a588e78c48a552cc0cff5
Tara Mathur <taramathur@gmail.com>
2017-06-12 09:25:30 -0400

    Added line to fix Heroku precompile error message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
   # Needed for Heroku.  Change host when in production!
   config.action_mailer.default_url_options = {:host => 'localhost:3000'}
 
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.serve_static_assets = true
+
 end
