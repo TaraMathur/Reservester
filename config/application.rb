3e2cbfecd299bafbc85c06dc36c333337c95310d
Tara Mathur <taramathur@gmail.com>
2017-06-12 09:07:50 -0400

    added config option for Heroku

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,9 @@ module Reservester
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+  # Required for Heroku
+  config.assets.initialize_on_precompile = false
+
   end
 
-  # Required for Heroku
-#  config.assets.initialize_on_precompile = false
 end
